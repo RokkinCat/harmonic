36c1eee70e1a73517094f80cd2016460a24729d6
Josh Holtz <josh@rokkincat.com>
2015-06-23 15:11:51 -0500

    Framework-ified maybe

---
+++ Harmonic-iOS/Harmonic-iOS.h
@@ -0,0 +1,19 @@
+//
+//  Harmonic-iOS.h
+//  Harmonic-iOS
+//
+//  Created by Josh Holtz on 6/23/15.
+//  Copyright (c) 2015 Josh Holtz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Harmonic-iOS.
+FOUNDATION_EXPORT double Harmonic_iOSVersionNumber;
+
+//! Project version string for Harmonic-iOS.
+FOUNDATION_EXPORT const unsigned char Harmonic_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Harmonic_iOS/PublicHeader.h>
+
+
