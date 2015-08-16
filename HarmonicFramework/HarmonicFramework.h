f149b035246a457d70767c0f0481505b5a140592
Josh Holtz <josh@rokkincat.com>
2015-08-16 00:16:33 -0500

    Carthage stuff

---
+++ HarmonicFramework/HarmonicFramework.h
@@ -0,0 +1,19 @@
+//
+//  HarmonicFramework.h
+//  HarmonicFramework
+//
+//  Created by Josh Holtz on 8/16/15.
+//  Copyright © 2015 Josh Holtz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HarmonicFramework.
+FOUNDATION_EXPORT double HarmonicFrameworkVersionNumber;
+
+//! Project version string for HarmonicFramework.
+FOUNDATION_EXPORT const unsigned char HarmonicFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HarmonicFramework/PublicHeader.h>
+
+
